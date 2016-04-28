77003367262160e5ae9d14d760ba5cde9fe723dd
Gilad Barkan <giladbarkan@live.com>
2016-04-28 22:15:52 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(arr,e)
+ arr.push(e)
+end
+
+def using_unshift(arr,e)
+  arr.unshift(e)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+# to index 4
+def using_insert(arr,e)
+  arr.insert(4,e)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr,e)
+  arr.delete(e)
+end
+
+def using_delete_at(arr,i)
+  arr.delete_at(i)
+end
